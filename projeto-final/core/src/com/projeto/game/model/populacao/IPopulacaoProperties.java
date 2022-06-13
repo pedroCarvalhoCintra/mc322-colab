a5eb8a72c808a86556adedfc1572448e7530a0da
Pedro Carvalho Cintra <pedrocintra2@gmail.com>
2022-06-13 18:17:26 -0300

    update files população

--- projeto-final/core/src/com/projeto/game/model/populacao/IPopulacaoProperties.java
+++ projeto-final/core/src/com/projeto/game/model/populacao/IPopulacaoProperties.java
@@ -1,5 +1,7 @@
 package com.projeto.game.model.populacao;
 
 public interface IPopulacaoProperties {
-
+    public int getNumHabitantes();
+    public int getSatisfacao();
+    public double getRendaPopulacao();
 }
--- projeto-final/core/src/com/projeto/game/model/populacao/Populacao.java
+++ projeto-final/core/src/com/projeto/game/model/populacao/Populacao.java
@@ -1,5 +1,26 @@
 package com.projeto.game.model.populacao;
 
 public class Populacao implements IPopulacao{
+    private int numHabitantes;
+    private int satisfacao;
+    private double rendaPopulacao;
+
+    public Populacao(int numHabitantesIniciais){
+        this.numHabitantes = numHabitantesIniciais;
+        this.satisfacao = 0;
+        this.satisfacao = 0;
+    }
+
+    public int getNumHabitantes(){
+        return numHabitantes;
+    }
+
+    public int getSatisfacao(){
+        return satisfacao;
+    }
+
+    public double getRendaPopulacao(){
+        return rendaPopulacao;
+    }
 
 }
