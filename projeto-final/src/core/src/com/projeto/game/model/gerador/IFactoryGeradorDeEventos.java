b347be4f9f1a27852f79db2c7e3dfe7cf36bc075
Pedro Carvalho Cintra <101885748+pedroCarvalhoCintra@users.noreply.github.com>
2022-06-28 10:44:00 -0300

    Delete IFactoryGeradorDeEventos.java

--- projeto-final/src/core/src/com/projeto/game/model/gerador/IFactoryGeradorDeEventos.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.game.model.gerador;
-
-import com.projeto.game.model.evento.IEvento;
-import com.projeto.game.model.construcao.IConstrucao;
-
-public interface IFactoryGeradorDeEventos {
-	public IEvento criarEventoAleatorio(int data);
-    public IEvento criarEventoConstrucao(IConstrucao construcao, int data);
-}
